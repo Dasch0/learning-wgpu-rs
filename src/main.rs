6e821e74b9707239ed58486051da5ae55eccf08f
David Schwarz <dsdavidschwarz@gmail.com>
2020-10-07 18:37:48 -0500

    WIP cube render

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use wgpu::util::DeviceExt;
 use winit::{
     event::*,
     event_loop::{EventLoop, ControlFlow},
     window::{Window, WindowBuilder},
 };
+use bytemuck::{Pod, Zeroable};
 
-struct Setup {
+struct Handle {
     instance: wgpu::Instance,
     size: winit::dpi::PhysicalSize<u32>,
     surface: wgpu::Surface,
@@ -13,7 +15,67 @@ struct Setup {
     queue: wgpu::Queue,
 }
 
-async fn setup (window: &Window) -> Setup {
+#[repr(C)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+struct Vertex {
+    _pos: [f32; 4],
+    _tex_coord:[f32; 2],
+}
+fn vertex(pos: [i8; 3], tc:[i8; 2]) -> Vertex {
+    Vertex {
+        _pos: [pos[0] as f32, pos[1] as f32, pos[2] as f32, 1.0],
+        _tex_coord: [tc[0] as f32, tc[1] as f32],
+
+    }
+}
+
+fn create_test_mesh() -> (Vec<Vertex>, Vec<u16>) {
+    let vertex_data = [
+        // top (0, 0, 1)
+        vertex([-1, -1, 1], [0, 0]),
+        vertex([1, -1, 1], [1, 0]),
+        vertex([1, 1, 1], [1, 1]),
+        vertex([-1, 1, 1], [0, 1]),
+        // bottom (0, 0, -1)
+        vertex([-1, 1, -1], [1, 0]),
+        vertex([1, 1, -1], [0, 0]),
+        vertex([1, -1, -1], [0, 1]),
+        vertex([-1, -1, -1], [1, 1]),
+        // right (1, 0, 0)
+        vertex([1, -1, -1], [0, 0]),
+        vertex([1, 1, -1], [1, 0]),
+        vertex([1, 1, 1], [1, 1]),
+        vertex([1, -1, 1], [0, 1]),
+        // left (-1, 0, 0)
+        vertex([-1, -1, 1], [1, 0]),
+        vertex([-1, 1, 1], [0, 0]),
+        vertex([-1, 1, -1], [0, 1]),
+        vertex([-1, -1, -1], [1, 1]),
+        // front (0, 1, 0)
+        vertex([1, 1, -1], [1, 0]),
+        vertex([-1, 1, -1], [0, 0]),
+        vertex([-1, 1, 1], [0, 1]),
+        vertex([1, 1, 1], [1, 1]),
+        // back (0, -1, 0)
+        vertex([1, -1, 1], [0, 0]),
+        vertex([-1, -1, 1], [1, 0]),
+        vertex([-1, -1, -1], [1, 1]),
+        vertex([1, -1, -1], [0, 1]),
+    ];
+
+    let index_data: &[u16] = &[
+        0, 1, 2, 2, 3, 0, // top
+        4, 5, 6, 6, 7, 4, // bottom
+        8, 9, 10, 10, 11, 8, // right
+        12, 13, 14, 14, 15, 12, // left
+        16, 17, 18, 18, 19, 16, // front
+        20, 21, 22, 22, 23, 20, // back
+    ];
+
+    (vertex_data.to_vec(), index_data.to_vec())
+}
+
+async fn setup (window: &Window) -> Handle {
     log::info!("Initializing instance...");
     let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
     
@@ -57,7 +119,7 @@ async fn setup (window: &Window) -> Setup {
         .unwrap();
     log::info!("Setup complete!");
 
-    Setup {
+    Handle {
         instance,
         size,
         surface,
@@ -68,14 +130,90 @@ async fn setup (window: &Window) -> Setup {
 }
 
 fn main() {
-    // Setup Winit
+    // Init Window
     env_logger::init();
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .build(&event_loop)
         .unwrap();
 
-    let setup = futures::executor::block_on(setup(&window));
+    // Init GPU Handle
+    let h = futures::executor::block_on(setup(&window));
+
+    log::info!("Initializing swapchain...");
+    let mut swapchain_desc = wgpu::SwapChainDescriptor {
+        usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+        format: wgpu::TextureFormat::Bgra8UnormSrgb,
+        width: h.size.width,
+        height: h.size.height,
+        present_mode: wgpu::PresentMode::Immediate,
+    };
+    let mut swap_chain = h
+        .device
+        .create_swap_chain(&h.surface, &swapchain_desc);
+
+    log::info!("Initializing Rendering Pipeline");
+    let bind_group_layout = h.device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        label: None,
+        entries: &[
+            wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStage::VERTEX,
+                ty: wgpu::BindingType::UniformBuffer {
+                    dynamic: false,
+                    min_binding_size: wgpu::BufferSize::new(64),
+                },
+                count: None,
+            },
+            wgpu::BindGroupLayoutEntry {
+                binding: 1,
+                visibility: wgpu::ShaderStage::FRAGMENT,
+                ty: wgpu::BindingType::SampledTexture {
+                    multisampled: false,
+                    component_type: wgpu::TextureComponentType::Float,
+                    dimension: wgpu::TextureViewDimension::D2,
+                },
+                count: None,
+            },
+            wgpu::BindGroupLayoutEntry {
+                binding: 2,
+                visibility: wgpu::ShaderStage::FRAGMENT,
+                ty: wgpu::BindingType::Sampler {
+                    comparison: false,
+                },
+                count: None,
+            },
+        ],
+    });
+
+    let pipeline_layout = h.device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: None,
+        bind_group_layouts: &[&bind_group_layout],
+        push_constant_ranges: &[],
+    });
+
+
+
+    //let scene_vert = h.device.create_shader_module(wgpu::include_spirv!("scene.vert.spv"));
+    //let scene_frag = h.device.create_shader_module(wgpu::include_spirv!("scene.frag.spv"));
+
+    log::info!("Initializing buffers...");
+    
+    let vertex_size = std::mem::size_of::<Vertex>();
+    let (vertex_data, index_data) = create_test_mesh();
+
+    let vertex_buf = h.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Vertex Buffer"),
+        contents: bytemuck::cast_slice(&vertex_data),
+        usage: wgpu::BufferUsage::VERTEX,
+    });
+
+    let index_buf = h.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Index Buffer"),
+        contents: bytemuck::cast_slice(&index_data),
+        usage: wgpu::BufferUsage::INDEX,
+    });
+
 
     event_loop.run(move |event, _, control_flow| {
         match event {
